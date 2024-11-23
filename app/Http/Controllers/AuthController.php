9b4a2ea83c893885268bee80d3298aa1216dd0c8
alexanderioo <frolov022a@gmail.com>
2024-11-23 09:21:20 +0300

    fix

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    protected $redirect = '/';
+    public function signup(){
+        return view('auth.signup');
+    }
+
+    public function registr(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required|min:6'
+        ]);
+        $response=[
+            'name'=>$request->name,
+            'email'=>request('email'),
+        ];
+        return response()->json($response);
+    }
+}
